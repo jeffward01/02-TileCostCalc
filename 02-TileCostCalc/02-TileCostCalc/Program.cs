e255644524e3d14c8475504a811350b7615bec6c
jeffward01 <jeffwardcoding@gmail.com>
2015-09-23 13:56:17 -0700

    Building Text

--- 02-TileCostCalc/02-TileCostCalc/Program.cs
+++ 02-TileCostCalc/02-TileCostCalc/Program.cs
@@ -10,6 +10,22 @@ namespace _02_TileCostCalc
     {
         static void Main(string[] args)
         {
+            //Ask if user is measuring tile for a Trianglur room, rectangle, or Circular
+            Console.WriteLine("Welcome to the Tile Cost Calcualtor Application!");
+            Console.WriteLine("Here we will evaluate the cost of your tile, and the dimsions of the room.");
+            Console.WriteLine("You will find out the total cost of the Tile, and the Sq Feet of the room you are in. \n \n ");
+            Console.WriteLine("Is your room a Rectangle/Square, Triangle or a Circle?");
+            Console.WriteLine("For Triangle Enter: Triangle");
+            Console.WriteLine("For a Square/Rectangle Enter: Rectangle");
+            Console.WriteLine("For a Circular room Enter: Circle");
+            string userInput = Console.ReadLine();
+            string input = userInput.ToLower();
+            
+            //Get input from user 
+            //Pending Users input execute either Rectangle, Tri, or Cir
+            //Return total sq feet, and cost of tile
+            //Draw a Square or Tri or Cir
+
         }
     }
 }
